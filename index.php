9a37b731235d6876e9f0b5737b4199a93f7522b8
AlexanderWaldetoft <32635983+AlexanderWaldetoft@users.noreply.github.com>
2018-04-26 08:53:23 +0200

    Update 26/4
    First commit
    
    Del 1

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+require ('resources/functions/functions.php');
+
+
+/*
+[Home]
+Inlägg (tidslinje)
+Kommentera (Endast om du är inloggad)
+Gilla
+Dela (Facebook,Twitter mm)
+
+[About]
+Min Profil
+Redigera Profil
+Användarnamn, Bild, om mig/om företag (beskrivning)
+Mina inlägg
+Redigera mina inlägg
+
+
+[Post] (Endast om du är inloggad)
+Skapa inlägg
+
+
+*/
+
+include ('resources/views/layout.php');
+
+
+ ?>
